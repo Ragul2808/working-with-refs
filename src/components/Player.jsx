ac3d77f18597a65387d61508fdfed1eceade3551
Ragul <ragulraja2808@gmail.com>
2024-05-20 12:27:46 +0530

    using Refs-in Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef} from "react";
 
 export default function Player() {
+  const playerName = useRef
   const [enteredPlayerName, setEnteredPlayerName] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+  
 
   function handleChange(event) {
     setSubmitted(false);
@@ -10,14 +11,17 @@ export default function Player() {
   }
 
   function handleClick() {
-    setSubmitted(true);
+   setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome { submitted ? enteredPlayerName : 'unknown entity' }</h2>
+      <h2>Welcome {  enteredPlayerName ?? 'unknown entity' }</h2>
       <p>
-        <input type="text" onChange={handleChange} />
+        <input 
+        ref={playerName}
+        type="text" 
+        onChange={handleChange} />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
